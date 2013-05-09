40649ac1442c998be0e614adfb3c3866a4e2c5cd
sergio sanes <sergio.sanes@gmail.com>
2013-05-09 17:05:15 -0500

    updating scripts

---
+++ project-data/scripts/stats.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./calculus/LG-params-medians-v2.out analysis-v4/0.0.1-LG-run-v4/10909/MW/milky-10909-0.0.1-LG-run-v4-props-z0.005-1.dat  analysis-v4/0.0.1-LG-run-v4/10909/MW/milky-10909-0.0.1-LG-run-v1-stats-z0.005.dat
+./calculus/LG-params-medians-v2.out analysis-v4/0.0.1-LG-run-v4/16953/MW/milky-16953-0.0.1-LG-run-v4-props-z0.005-1.dat  analysis-v4/0.0.1-LG-run-v4/16953/MW/milky-16953-0.0.1-LG-run-v1-stats-z0.005.dat
+./calculus/LG-params-medians-v2.out analysis-v4/0.0.1-LG-run-v4/2710/MW/milky-2710-0.0.1-LG-run-v4-props-z0.005-1.dat  analysis-v4/0.0.1-LG-run-v4/2710/MW/milky-2710-0.0.1-LG-run-v1-stats-z0.005.dat
+
+
+./calculus/LG-params-medians-v2.out analysis-v4/0.0.1-LG-run-v4/10909/M31/m31-10909-0.0.1-LG-run-v4-props-z0.005-1.dat  analysis-v4/0.0.1-LG-run-v4/10909/M31/m31-10909-0.0.1-LG-run-v1-stats-z0.005.dat
+./calculus/LG-params-medians-v2.out analysis-v4/0.0.1-LG-run-v4/16953/M31/m31-16953-0.0.1-LG-run-v4-props-z0.005-1.dat  analysis-v4/0.0.1-LG-run-v4/16953/M31/m31-16953-0.0.1-LG-run-v1-stats-z0.005.dat
+./calculus/LG-params-medians-v2.out analysis-v4/0.0.1-LG-run-v4/2710/M31/m31-2710-0.0.1-LG-run-v4-props-z0.005-1.dat  analysis-v4/0.0.1-LG-run-v4/2710/M31/m31-2710-0.0.1-LG-run-v1-stats-z0.005.dat
+
+
